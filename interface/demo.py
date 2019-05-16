908c798534e042601ed4b5692876dd81a41f8102
Boris <mrowoxd@gmail.com>
2019-05-16 19:40:35 -0400

    Hippopede

--- interface/demo.py
+++ interface/demo.py
@@ -11,13 +11,14 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
 
 # Prepare arrays x, y, z
-theta = np.linspace(-4 * np.pi, 4 * np.pi, 999)
-z = np.linspace(-2, 2, 100)
-r = z**2 + 1
-x = r * np.sin(theta)
-y = r * np.cos(theta)
-
+theta = np.linspace(-4 * np.pi, 4 * np.pi,99)
+a = 1
+r= 5
+x = a + (r-a) * np.cos(theta)
+y = (r-a) * np.sin(theta)
+z = 2*(a*(r-a))** (1/2) * np.sin(theta/2)
 ax.plot(x, y, z, label='parametric curve')
+
 ax.legend()
 
 plt.show()
